33baadd38208f02fc65fae56b7eb2247e72c3b8d
ilyaas <ilyaasbaker3@gmail.com>
2022-12-10 19:34:34 +0400

    Updated

--- app/src/main/java/com/example/ilyaasbaker_bmiapp/MainActivity.kt
+++ app/src/main/java/com/example/ilyaasbaker_bmiapp/MainActivity.kt
@@ -10,16 +10,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val weight=findViewById(R.id.editTextNumber) as EditText
-        val height=findViewById(R.id.editTextNumber2) as EditText
-        val bmi=findViewById(R.id.textView2) as TextView
-        val btn=findViewById(R.id.button) as Button
+        val weight= findViewById<EditText>(R.id.editTextNumber)
+        val height= findViewById<EditText>(R.id.editTextNumber2)
+        val bmi= findViewById<TextView>(R.id.textView2)
+        val btn= findViewById<Button>(R.id.button)
         btn.setOnClickListener{
             val weight1=weight.text.toString().toInt()
             val height1=height.text.toString().toInt()
 
-            val bmiformula=(weight1/height1/height1)*10000;
-            bmi.setText("Your BMI(Body Mass Index) is "+bmiformula.toString())
+            val formula=(weight1/height1/height1)*10000
+            bmi.setText("Your BMI(Body Mass Index) is "+formula.toString())
+
         }
     }
 }
